8c410c4f8e069e3e9668cbe3dfe592f39b203c0f
Alex Balderson <ptmmr.black@gmail.com>
2017-10-23 09:39:33 -0600

    next round of updates

--- lib/regional.py
+++ lib/regional.py
@@ -7,9 +7,10 @@ class Regional(object):
 	"""docstring for Regional"""
 	def __init__(self, path):
 		self.name = os.path.basename(os.path.normpath(path))
+		self._path = path
 		for file in os.listdir(path):
 			if file.endswith('.staticdata'):
-				content_file = os.path.join(path, 'solarsystem.staticdata')
+				content_file = os.path.join(path, file)
 				self._content = yaml.load(open(content_file))
 				break
 
--- lib/solarsystem.py
+++ lib/solarsystem.py
@@ -10,6 +10,9 @@ class Solarsystem(regional.Regional):
 	def __init__(self, path_to_solarsystem):
 		regional.Regional.__init__(self, path_to_solarsystem)
 		self.name_id = self._content['solarSystemNameID']
+		self.id = self._content['solarSystemID']
+		self.security = round(self._content['security'], 1)
+		# will need station data eventually \
 
 
 if __name__ == '__main__':
@@ -18,3 +21,5 @@ if __name__ == '__main__':
 	test_system = Solarsystem(solarsystem)
 	print(test_system.name)
 	print(test_system.name_id)
+	print(test_system.id)
+	print(test_system.security)
