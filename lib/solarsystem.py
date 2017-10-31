1f0091cf3f20e59b5272199c25a38cf43b64da87
Alex Balderson <ptmmr.black@gmail.com>
2017-10-30 21:46:39 -0600

    import universe into mongodb

--- lib/solarsystem.py
+++ lib/solarsystem.py
@@ -6,26 +6,26 @@ import yaml
 import regional
 
 class Solarsystem(regional.Regional):
-	"""TODO"""
-	def __init__(self, path_to_solarsystem):
-		regional.Regional.__init__(self, path_to_solarsystem)
-		self.name_id = self._content['solarSystemNameID']
-		self.id = self._content['solarSystemID']
-		self.security = round(self._content['security'], 1)
-		# will need station data eventually \
+    """TODO"""
+    def __init__(self, path_to_solarsystem):
+        regional.Regional.__init__(self, path_to_solarsystem)
+        self.name_id = self._content['solarSystemNameID']
+        self.id = self._content['solarSystemID']
+        self.security = round(self._content['security'], 1)
+        # will need station data eventually \
 
 
-	def __str__(self):
-		#TODO dump to dictionary/json
-		pass
+    def __str__(self):
+    #TODO dump to dictionary/json
+        pass
 
 
 if __name__ == '__main__':
-	import config
-	solarsystem = os.path.join(config.SDE_PATH, 'fsd/universe/eve/BlackRise/Aokinen/Ahtila')
-	test_system = Solarsystem(solarsystem)
-	print(test_system.name)
-	print(test_system.name_id)
-	print(test_system.id)
-	print(test_system.security)
-	print(test_system.to_dict())
+    import config
+    solarsystem = os.path.join(config.SDE_PATH, 'fsd/universe/eve/BlackRise/Aokinen/Ahtila')
+    test_system = Solarsystem(solarsystem)
+    print(test_system.name)
+    print(test_system.name_id)
+    print(test_system.id)
+    print(test_system.security)
+    print(test_system.to_dict())
---
+++ lib/universe2mongo.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+import os
+from pymongo import MongoClient
+
+import config
+import universe
+
+
+def main(args):
+    #this takes a really long time, like 20 min
+    eve_universe = universe.Universe(os.path.join(config.SDE_PATH, 'fsd/universe/eve'))
+    #eve_universe = universe.Universe(os.path.join(config.SDE_PATH, 'test'))
+    mongo_client = MongoClient('mongodb://%s:27017' % config.MONGO_IP)
+    db = mongo_client[config.MONGODB]
+    eveverse = db[config.MONGOUNIVERSE]
+    print(type(eveverse))
+    for region in eve_universe.regions.keys():
+        #print('we have a region')
+        for const in eve_universe.regions[region].constellations.keys():
+            #print('we have a constellation')
+            for solar in eve_universe.regions[region].constellations[const].solarsystems.keys():
+                #print('we have a solarsystem')
+                record = eve_universe.regions[region].constellations[const].solarsystems[solar].to_dict()
+                record['regionID'] = eve_universe.regions[region].id
+                record['regionName'] = region
+                record['constellationID'] = eve_universe.regions[region].constellations[const].id
+                record['constellationName'] = const
+                #it didnt insert....
+                #print(record)
+                temp = eveverse.insert_one(record)
+                #why is it not there?
+                print(temp.acknowledged)
+                print(temp.inserted_id)
+    mongo_client.close()
+
+
+if __name__ == '__main__':
+    main(None)
