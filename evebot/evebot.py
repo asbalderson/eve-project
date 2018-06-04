74887609e46a4561106cfb7409513d5324199568
Alex Balderson <alex@menutrinfo.com>
2018-06-04 11:56:58 -0600

    adding timezone conversion functionality

--- evebot/evebot.py
+++ evebot/evebot.py
@@ -10,6 +10,7 @@ from discord.ext import commands
 from . import __version__
 
 from .commands import travelslut
+from .commands import timeslut
 from .utility.discord_argparse import DiscordArgparser
 
 BOT = commands.Bot(command_prefix='-', description='Eve utils, -help for help')
@@ -39,7 +40,9 @@ async def on_message_edit(before, after):
 async def call_travelslut(ctx):
     await BOT.type()
     parser = DiscordArgparser(bot=BOT,
-                              description='Find distance between 2 systems',
+                              description='Find distance between 2 systems.  '
+                                          'Systems with 2 word names need to be'
+                                          'wrapped in "quotes".',
                               usage='-travelslut source destination [-i] [-s]')
     parser.add_argument('source', help='System leaving from')
     parser.add_argument('destination', help='Where you are going or '
@@ -48,7 +51,7 @@ async def call_travelslut(ctx):
                         help='Systems to ignore.  Can be specified more than once',
                         action='append')
     parser.add_argument('-s', '--security',
-                        help='Security prefference for route.  '
+                        help='Security preference for route.  '
                              'One of "shortest", "secure", or "insecure" \n'
                              '\t default=shortest',
                         default='shortest')
@@ -63,6 +66,22 @@ async def call_travelslut(ctx):
                                       args.destination,
                                       args.ignore,
                                       security=args.security))
+
+
+@BOT.command(name='timeslut', pass_context=True)
+async def call_timeslut(ctx):
+    await BOT.type()
+    parser = DiscordArgparser(bot=BOT,
+                              description='Calculate timezone differences from eve time',
+                              usage='-timeslut time timezone')
+    parser.add_argument('time', help='The time to convert, in the format HHMM '
+                                     'as military time i.e. 0830, 1545')
+    parser.add_argument('timezone', help='Timezone abbreviations, one of: \n%s'
+                                         % ', '.join(timeslut.TZ_DICT.keys()))
+
+    raw_args = shlex.split(ctx.message.content)
+    args = parser.parse_args(raw_args[1:])
+    timeslut.main(args)
 
 
 @BOT.command(hidden=True)
